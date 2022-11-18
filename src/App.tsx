90f63ccccdf656cf6c1bc1c5784ae463fc88c2bb
iguana3d <igor.cordeiro2016@outlook.com>
2022-11-17 23:36:02 -0300

    feature: integração com a API covid e implementação do theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import MainRoutes from "./routes";
+import { ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "./theme";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  SettingsConsumer,
+  SettingsProvider,
+} from "./contexts/settings-context";
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
-      <Router>
-        <MainRoutes />
-      </Router>
+      <SettingsProvider>
+        <SettingsConsumer>
+          {({ settings }) => (
+            <ThemeProvider
+              theme={createTheme({
+                direction: settings.direction,
+                responsiveFontSizes: settings.responsiveFontSizes,
+                mode: settings.theme,
+              })}
+            >
+              <CssBaseline />
+              <Router>
+                <MainRoutes />
+              </Router>
+            </ThemeProvider>
+          )}
+        </SettingsConsumer>
+      </SettingsProvider>
     </React.Fragment>
   );
 };
--- src/pages/dashboard/Home/index.tsx
+++ src/pages/dashboard/Home/index.tsx
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from "react";
-import Chart from "react-apexcharts";
-import type { ApexOptions } from "apexcharts";
-//Components
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
-  Typography,
+  FormControl,
+  Grid,
+  MenuItem,
   Paper,
   TextField,
-  MenuItem,
-  Grid,
+  Typography,
 } from "@mui/material";
-import { Dayjs } from "dayjs";
+import { useTheme } from "@mui/material/styles";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import type { ApexOptions } from "apexcharts";
+import dayjs, { Dayjs } from "dayjs";
+import Chart from "react-apexcharts";
 import TableList from "./TableList";
 //Services
 import { getCoronavirusCases } from "../../services/coronavirus.service";
@@ -22,6 +24,7 @@ import { getCoronavirusCases } from "../../services/coronavirus.service";
 import { ICoronaVirusCases } from "../../services/coronavirus.types";
 
 const unitFederations = [
+  { value: "", label: "Selecione" },
   { value: "AC", label: "AC" },
   { value: "AL", label: "AL" },
   { value: "AP", label: "AP" },
@@ -56,10 +59,9 @@ const Home: React.FC = () => {
   const [dateCases, setDateCases] = useState<Dayjs | null>(null);
   const [coronaVirusCasesData, setCoronaVirusCasesData] =
     useState<ICoronaVirusCases>({} as ICoronaVirusCases);
+  const theme = useTheme();
 
-  const handleUnitFederationChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleUnitFederationChange = (event: SelectChangeEvent) => {
     setUnitFederation(event.target.value);
   };
 
@@ -133,91 +135,141 @@ const Home: React.FC = () => {
 
   return (
     <Box>
-      <Typography variant="h2">Covid-19</Typography>
+      <Typography variant="h3">Covid-19</Typography>
       <Typography variant="body1">
         Boletins informativos e casos de coronavírus por município por dia
       </Typography>
-      <Paper sx={{ p: 3, mt: 3 }} elevation={3}>
-        <Typography variant="h2">Relatório de Casos</Typography>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Box component="span">UF</Box>
-          <TextField
-            select
-            value={unitFederation}
-            fullWidth
-            onChange={handleUnitFederationChange}
+      <Paper
+        sx={{ p: 3, mt: 3, display: "flex", flexDirection: "column", gap: 3 }}
+        elevation={3}
+      >
+        <Typography variant="h3">Relatório de Casos</Typography>
+        <Box sx={{ display: "flex", gap: 3 }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: 400,
+              width: "100%",
+              gap: 1,
+            }}
           >
-            {unitFederations.map((uf) => (
-              <MenuItem key={uf.value} value={uf.value}>
-                {uf.label}
-              </MenuItem>
-            ))}
-          </TextField>
+            <Box component="span">UF</Box>
+            <FormControl fullWidth>
+              <Select
+                value={unitFederation}
+                onChange={handleUnitFederationChange}
+                fullWidth
+                displayEmpty
+              >
+                {unitFederations.map((uf) => (
+                  <MenuItem key={uf.value} value={uf.value}>
+                    {uf.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            {/* <TextField
+              select
+              value={unitFederation}
+              onChange={handleUnitFederationChange}
+              fullWidth
+            >
+              {unitFederations.map((uf) => (
+                <MenuItem key={uf.value} value={uf.value}>
+                  {uf.label}
+                </MenuItem>
+              ))}
+            </TextField> */}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: 400,
+              width: "100%",
+              gap: 1,
+            }}
+          >
+            <Box component="span">Data</Box>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                inputFormat="DD/MM/YYYY"
+                maxDate={dayjs(Date.now())}
+                value={dateCases}
+                onChange={(newValue) => {
+                  setDateCases(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Box>
         </Box>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Box component="span">Data</Box>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              value={dateCases}
-              onChange={(newValue) => {
-                setDateCases(newValue);
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </Box>
-        <Button onClick={getCoronavirusCasesData} variant="contained">
+        <Button
+          onClick={getCoronavirusCasesData}
+          variant="contained"
+          sx={{ width: "max-content" }}
+        >
           Mostrar dados
         </Button>
       </Paper>
-      <Paper sx={{ p: 3, mt: 3 }} elevation={3}>
-        <Typography variant="body1">
-          Distribuição de casos de COVID-19
-        </Typography>
-        <Chart
-          height={200}
-          options={chartOptions}
-          series={data.series.map((item) => item.data)}
-          type="donut"
-        />
-        <Grid container>
-          {data.series.map((item) => (
-            <Grid
-              item
-              key={item.label}
-              sx={{
-                alignItems: "center",
-                display: "flex",
-                p: 1,
-              }}
-              xs={6}
-            >
-              <Box
-                sx={{
-                  backgroundColor: item.color,
-                  borderRadius: "50%",
-                  height: 16,
-                  mr: 1,
-                  width: 16,
-                }}
-              />
-              <Typography variant="subtitle2">{item.label}</Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Paper sx={{ p: 3, mt: 3 }} elevation={3}>
+            <Typography variant="body1">
+              Distribuição de casos de COVID-19
+            </Typography>
+            <Chart
+              height={200}
+              options={chartOptions}
+              series={data.series.map((item) => item.data)}
+              type="donut"
+            />
+            <Grid container>
+              {data.series.map((item) => (
+                <Grid
+                  item
+                  key={item.label}
+                  sx={{
+                    alignItems: "center",
+                    display: "flex",
+                    p: 1,
+                  }}
+                  xs={6}
+                >
+                  <Box
+                    sx={{
+                      backgroundColor: item.color,
+                      borderRadius: "50%",
+                      height: 16,
+                      mr: 1,
+                      width: 16,
+                    }}
+                  />
+                  <Typography variant="subtitle2">{item.label}</Typography>
+                </Grid>
+              ))}
             </Grid>
-          ))}
+          </Paper>
+          <Typography variant="body2" color={theme.palette.error.main} sx={{ mt: 2, ml: .5 }}>
+            Distribuição de porcentagem dos municípios que tem confirmações no
+            dia diferente de 0
+          </Typography>
         </Grid>
-      </Paper>
-      <Typography variant="body2">
-        Distribuição de porcentagem dos municípios que tem confirmações no dia
-        diferente de 0
-      </Typography>
-      <Paper sx={{ p: 3, mt: 3 }} elevation={3}>
-        <Typography variant="body1">Lista de casos por município</Typography>
-        <TableList coronaVirusCasesData={coronaVirusCasesData} />
-      </Paper>
-      <Typography variant="body2">
-        Distribuição de municípios que tem confirmações no dia diferente de 0
-        ordenados de mais confirmações no dia para menos confirmações no dia
-      </Typography>
+        <Grid item xs={12} md={6}>
+          <Paper sx={{ p: 3, mt: 3 }} elevation={3}>
+            <Typography variant="body1">
+              Lista de casos por município
+            </Typography>
+            <TableList coronaVirusCasesData={coronaVirusCasesData} />
+          </Paper>
+          <Typography variant="body2" color={theme.palette.error.main} sx={{ mt: 2, ml: .5 }}>
+            Distribuição de municípios que tem confirmações no dia diferente de
+            0 ordenados de mais confirmações no dia para menos confirmações no
+            dia
+          </Typography>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
